070637f887ad957a1f98e0e1516fc4e96d321276
SINtyanneru <irjitgd@gmail.com>
2024-02-25 17:08:50 +0900

    ダウンロード済みなら処理しない

--- src/main/java/com/rumisystem/pixiv_dlp/GET/ILLUST_GET.java
+++ src/main/java/com/rumisystem/pixiv_dlp/GET/ILLUST_GET.java
@@ -4,7 +4,9 @@ import com.fasterxml.jackson.databind.JsonNode;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.rumisystem.pixiv_dlp.DIR;
 import com.rumisystem.pixiv_dlp.HTTP_REQUEST;
+import com.rumisystem.pixiv_dlp.LOG_TYPE;
 
+import java.io.File;
 import java.util.Date;
 
 import static com.rumisystem.pixiv_dlp.Main.*;
@@ -27,39 +29,45 @@ public class ILLUST_GET {
 					String ILLUST_U_DATE = BODY_JSON.get("uploadDate").asText();
 					String AUTHOR_ID = BODY_JSON.get("userId").asText();
 
-					//適当に情報を吐く
-					LOG(2, "┌──────────────────────────────────────────┐");
-					LOG(2, "│ﾀｲﾄﾙ      :" + ILLUST_TITLE);
-					LOG(2, "│ﾍﾟｰｼﾞ     :" + ILLUST_PAGE_COUNT);
-					LOG(2, "│ｻｸｾｲﾋﾞ    :" + ILLUST_C_DATE);
-					LOG(2, "│ｺｳｼﾝﾋﾞ    :" + ILLUST_U_DATE);
-					LOG(2, "└──────────────────────────────────────────┘");
+					File ILLUST_FILE = new File("pixiv/" + AUTHOR_ID + "/" + ILLUST_ID);
+					if(!ILLUST_FILE.exists()){
+						//適当に情報を吐く
+						LOG(2, "┌──────────────────────────────────────────┐");
+						LOG(2, "│ﾀｲﾄﾙ      :" + ILLUST_TITLE);
+						LOG(2, "│ﾍﾟｰｼﾞ     :" + ILLUST_PAGE_COUNT);
+						LOG(2, "│ｻｸｾｲﾋﾞ    :" + ILLUST_C_DATE);
+						LOG(2, "│ｺｳｼﾝﾋﾞ    :" + ILLUST_U_DATE);
+						LOG(2, "└──────────────────────────────────────────┘");
 
-					String AJAX_ILLUST_ALL_PAGE = new HTTP_REQUEST("https://www.pixiv.net/ajax/illust/" + ILLUST_ID + "/pages?lang=ja").GET();
+						String AJAX_ILLUST_ALL_PAGE = new HTTP_REQUEST("https://www.pixiv.net/ajax/illust/" + ILLUST_ID + "/pages?lang=ja").GET();
 
-					//nullちぇっく
-					if(AJAX_ILLUST_ALL_PAGE != null){
-						ObjectMapper ALL_PAGE_OBJ_MAPPER = new ObjectMapper();
-						JsonNode ILLUST_ALL_PAGE_JSON = ALL_PAGE_OBJ_MAPPER.readTree(AJAX_ILLUST_ALL_PAGE);
+						//nullちぇっく
+						if(AJAX_ILLUST_ALL_PAGE != null){
+							ObjectMapper ALL_PAGE_OBJ_MAPPER = new ObjectMapper();
+							JsonNode ILLUST_ALL_PAGE_JSON = ALL_PAGE_OBJ_MAPPER.readTree(AJAX_ILLUST_ALL_PAGE);
 
-						//ページ枚数分回す
-						for(int I = 0; I < ILLUST_PAGE_COUNT; I++){
-							//画像のURL(_p0をページ番号におきかえている)
-							String ORIGIN_ILLUST_URL = ILLUST_ALL_PAGE_JSON.get("body").get(I).get("urls").get("original").asText();
+							//ページ枚数分回す
+							for(int I = 0; I < ILLUST_PAGE_COUNT; I++){
+								//画像のURL(_p0をページ番号におきかえている)
+								String ORIGIN_ILLUST_URL = ILLUST_ALL_PAGE_JSON.get("body").get(I).get("urls").get("original").asText();
 
-							//ディレクトリを作成する
-							new DIR(AUTHOR_ID, ILLUST_ID, BODY_JSON);
+								//ディレクトリを作成する
+								new DIR(AUTHOR_ID, ILLUST_ID, BODY_JSON);
 
-							//ページをダウンロード(分かりやすくファイルのページ番号を+1する)
-							new HTTP_REQUEST(ORIGIN_ILLUST_URL).DOWNLOAD("pixiv/" + AUTHOR_ID + "/" + ILLUST_ID + "/" + (I + 1) + ".png");
+								//ページをダウンロード(分かりやすくファイルのページ番号を+1する)
+								new HTTP_REQUEST(ORIGIN_ILLUST_URL).DOWNLOAD("pixiv/" + AUTHOR_ID + "/" + ILLUST_ID + "/" + (I + 1) + ".png");
+							}
+
+							OK_JOB++;
+							return true;
+						} else {
+							LOG(1, "処理をスキップします");
+							FAILED_JOB++;
+							return false;
 						}
-
-						OK_JOB++;
-						return true;
 					} else {
-						LOG(1, "処理をスキップします");
-						FAILED_JOB++;
-						return false;
+						LOG(LOG_TYPE.OK, "すでに存在するのでスキップしました");
+						return true;
 					}
 				}else {
 					LOG(1, "APIエラー「" + AJAX_RESULT_JSON.get("message").asText() + "」");
